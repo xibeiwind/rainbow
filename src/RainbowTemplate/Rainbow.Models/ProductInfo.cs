37cfa53419b856239fd44adad1fc1d9077009247
Alex Chen <xibeiwind@126.com>
2019-10-04 09:39:05 +0800

    issuefix

--- src/RainbowTemplate/Rainbow.Models/ProductInfo.cs
+++ src/RainbowTemplate/Rainbow.Models/ProductInfo.cs
@@ -11,7 +11,20 @@ namespace Rainbow.Models
     [Table(nameof(ProductInfo))]
     public class ProductInfo : Entity
     {
+        /// <summary>
+        ///     商品名称
+        /// </summary>
+        [Display(Name = "商品名称")]
         public string Name { get; set; }
+        /// <summary>
+        ///     商品描述
+        /// </summary>
+        [Display(Name = "商品描述")]
         public string Description { get; set; }
+        /// <summary>
+        ///     商品价格
+        /// </summary>
+        [Display(Name = "商品价格")]
+        public decimal Price { get; set; }
     }
 }
