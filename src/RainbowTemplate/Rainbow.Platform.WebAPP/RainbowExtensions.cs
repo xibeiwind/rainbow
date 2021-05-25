e93a3845bd127f5a02b1674cd0cfb6a0f7a91bef
Alex <xibeiwind@126.com>
2021-05-25 22:49:07 +0800

    upgrade to .net5 and update packages

--- src/RainbowTemplate/Rainbow.Platform.WebAPP/RainbowExtensions.cs
+++ src/RainbowTemplate/Rainbow.Platform.WebAPP/RainbowExtensions.cs
@@ -1,4 +1,6 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
 
 using Microsoft.AspNetCore.Builder;
@@ -46,7 +48,8 @@ namespace Rainbow.Platform.WebAPP
 
             services.AddDbContext<RainbowDbContext>(opts =>
             {
-                opts.UseMySql(configuration.GetConnectionString("RainbowDB"));
+                var serverVersion = new MySqlServerVersion(new Version(8, 0, 21));
+                opts.UseMySql(configuration.GetConnectionString("RainbowDB"), serverVersion);
             });
 
             services.AddSingleton<IEntityRegisterService, RainbowEntityRegisterService>();
--- src/RainbowTemplate/Rainbow.Schemas/RainbowMutation.cs
+++
@@ -1,8 +0,0 @@
-﻿using GraphQL.Types;
-
-namespace Rainbow.Schemas
-{
-    public class RainbowMutation : ObjectGraphType
-    {
-    }
-}
